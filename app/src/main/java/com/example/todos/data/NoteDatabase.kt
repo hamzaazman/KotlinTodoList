ce8dfe905241b61deaafd552b216dfebd62571fd
Hamza Azman <hamzaazman@outlook.com>
2022-12-24 22:20:29 +0300

    migrate new db version

--- app/src/main/java/com/example/todos/data/NoteDatabase.kt
+++ app/src/main/java/com/example/todos/data/NoteDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.todos.model.Note
 
-@Database(entities = [Note::class], version = 1, exportSchema = false)
+@Database(entities = [Note::class], version = 2, exportSchema = false)
+@TypeConverters(Converter::class)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
@@ -16,12 +18,8 @@ abstract class NoteDatabase : RoomDatabase() {
         fun getDatabase(context: Context): NoteDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    NoteDatabase::class.java,
-                    "note_database"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
+                    context.applicationContext, NoteDatabase::class.java, "note_database"
+                ).fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 return instance
             }
